6a75c9e62f6ec11e3dc37f4a18b5dec6d1ee41f4
sungyup-kim <sungyup@mz.co.kr>
2024-07-08 02:08:37 +0000

    handler

--- pytorch_tutorial/no_prepost_processing_handler.py
+++ pytorch_tutorial/no_prepost_processing_handler.py
@@ -7,26 +7,15 @@ class NoPrePostProcessingHandler(BaseHandler):
         self.manifest = context.manifest
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	
-        print(context.system_properties)
-        #model_dir = "/mnt/models/model-store" #context.system_properties.get("model_store")
-        #print("dir : " + str(model_dir))
         self.model = self.load_model(context.system_properties.get("model_dir"))
         self.model.eval()
-        #self.model = model
+        self.initialized = True
 
     def load_model(self, model_dir):
-        #model = torch.load(model_dir+"/mnist.pt")
         model = torch.jit.load(model_dir+"/mnist.pt")
         return model
-    #    import os
-    #    from ts.torch_handler.base_handler import _load_pickled_model
-
-    #    model_path = os.path.join(model_dir, "model.pth")
-    #    model = _load_pickled_model(model_path)
-    #    return model
 
     def preprocess(self, data):
-        # No preprocessing
         return data
 
     def inference(self, data, *args, **kwargs):
@@ -40,4 +29,8 @@ class NoPrePostProcessingHandler(BaseHandler):
 
     def postprocess(self, data):
         # No postprocessing
-        return data
+        results = []
+        for output in data:
+            _, predicted = torch.max(output, 0)
+            results.append(predicted.item())
+        return results
